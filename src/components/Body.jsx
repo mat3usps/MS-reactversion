7a8211cfc2480ade2b234325e745bc60ef2ff482
Mateus Pereira <mateusp.s@outlook.com>
2021-03-02 23:49:37 -0300

    added: Homepage(to fix:background,svg-animation)

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,5 +1,62 @@
 function Body() {
-  return <h1>Meu Site</h1>;
+  return (
+    <div>
+      <div class="container">
+        <div class="row buttonbar">
+          <div class="col-2">
+            <a href="coding.html">
+              <div class="btn-sp btn-three">
+                <span>Coding</span>
+              </div>
+            </a>
+          </div>
+          <div class="col-2">
+            <a href="photos.html">
+              <div class="btn-sp btn-three">
+                <span>Photos</span>
+              </div>
+            </a>
+          </div>
+          <div class="col-2">
+            <a href="about.html">
+              <div class="btn-sp btn-three">
+                <span>About</span>
+              </div>
+            </a>
+          </div>
+          <div class="col-2">
+            <a href="games.html">
+              <div class="btn-sp btn-three">
+                <span>Games</span>
+              </div>
+            </a>
+          </div>
+          <div class="col-2">
+            <a href="paintings.html">
+              <div class="btn-sp btn-three">
+                <span>Paintings</span>
+              </div>
+            </a>
+          </div>
+          <div class="col-2">
+            <a href="musings.html">
+              <div class="btn-sp btn-three">
+                <span>Musings</span>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="loader-wrapper">
+        <a href="../index.html">
+          <div class="animated-svg"></div>
+        </a>
+      </div>
+
+      <script type="text/javascript"> ChangeIt(); </script>
+    </div>
+  );
 }
 
 export default Body;
