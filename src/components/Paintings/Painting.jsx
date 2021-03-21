7023e396c76f4c3ac16b24abe1d91b4ff4c99c85
Mateus Pereira <mateusp.s@outlook.com>
2021-03-21 03:14:16 -0300

    fixed: paintings-display added:eggs & cartbutton

--- src/components/Paintings/Painting.jsx
+++ src/components/Paintings/Painting.jsx
@@ -1,13 +1,55 @@
-function Painting({ image, children, description, price }) {
+import { useState } from "react";
+import Arrow from "./arrow.svg";
+
+function Painting({
+  image,
+  children,
+  description,
+  price,
+  superiorAction,
+  inferiorAction,
+  titleEgg,
+  priceAction,
+}) {
+  const [showingEgg, manipulateEgg] = useState(false);
+  const displayTitleEgg = () => {
+    if (showingEgg === false) {
+      manipulateEgg(true);
+    } else {
+      manipulateEgg(false);
+    }
+  };
+
   return (
     <div className="painting">
       <div className="frame">
+        <button
+          className="superior-arrow-button arrow-button"
+          onClick={superiorAction}
+        >
+          <img src={Arrow} alt="superior" />
+        </button>
+        <br />
         <img className="paintingImage" src={image} alt={children} />
+        <br />
+        <button
+          className="inferior-arrow-button arrow-button"
+          onClick={inferiorAction}
+        >
+          <img src={Arrow} alt="inferior" />
+        </button>
       </div>
       <div className="frameInfo">
-        <h2>{children}</h2>
+        <button className="painting-title-egg-button" onClick={displayTitleEgg}>
+          <h2>{children}</h2>
+        </button>
+        {showingEgg && (
+          <img className="painting-egg" src={titleEgg} alt={titleEgg} />
+        )}
         <p>{description}</p>
-        <h3>{price}</h3>
+        <button className="cart-button" type="text" onClick={priceAction}>
+          {price}
+        </button>
       </div>
     </div>
   );
--- src/components/Paintings/Paintings.jsx
+++ src/components/Paintings/Paintings.jsx
@@ -1,37 +1,60 @@
 import Painting from "./Painting";
 import P1 from "./P-1.jpeg";
 import P2 from "./P-2.jpg";
+import { useState } from "react";
 
 function Paintings() {
   const paintings = [
     {
       name: "Varanda",
       description:
-        "The first ever frame painted by Mateus Pereira. The range of colors depicts the exact moment in which the wish of painting became reality. The shapes and colors bring about the views from the author's porch.",
-      price: "$ 700.00",
+        "The first ever frame painted by Mateus Pereira. The shapes and colors bring about the views from the author's porch. Such venture manifest in itself a range of technique experimentation brought out of pure curiosity.",
+      price: "$ 800.00",
       image: P1,
+      photo: P1,
     },
     {
       name: "Pulo",
       description:
-        "A frame to encapsulate the true insanity contained in the mind of a parachutist who gazes down the enormous solid ground from over the clouds and throws oneself to the freeing fall.",
+        "Based on the author's first jump. That's a frame to encapsulate the insanity hidden in the mind of a parachutist who gazes down the enormous solid ground from over the clouds and throws oneself to the freeing fall.",
       price: "$ 650.00",
       image: P2,
+      photo: "",
     },
   ];
 
+  const [paintingIndex, changePainting] = useState(0);
+  const superiorPainting = () => {
+    if (paintingIndex === 0) {
+      changePainting(paintings.length - 1);
+    } else {
+      changePainting(paintingIndex - 1);
+    }
+  };
+
+  const inferiorPainting = () => {
+    if (paintingIndex === paintings.length - 1) {
+      changePainting(0);
+    } else {
+      changePainting(paintingIndex + 1);
+    }
+  };
+
+  let currentPainting = paintings[paintingIndex];
+
   return (
     <div>
-      {paintings.map(({ name, description, price, image }) => (
-        <Painting
-          description={description}
-          price={price}
-          image={image}
-          key={name}
-        >
-          {name}
-        </Painting>
-      ))}
+      <Painting
+        superiorAction={superiorPainting}
+        inferiorAction={inferiorPainting}
+        description={currentPainting.description}
+        price={currentPainting.price}
+        image={currentPainting.image}
+        titleEgg={currentPainting.photo}
+        priceAction={""}
+      >
+        {currentPainting.name}
+      </Painting>
     </div>
   );
 }
